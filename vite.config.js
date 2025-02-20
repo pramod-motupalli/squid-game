1cd62c6272dc66271f3d2554c6a1c413cddd293d
pramod-motupalli <motupallisnmsdurgapramod531@gmail.com>
2025-02-20 22:33:28 +0530

    level2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),react()
-  ],
-})
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(async () => {
+  const tailwindcss = (await import("@tailwindcss/vite")).default;
+
+  return {
+    plugins: [react(), tailwindcss()],
+  };
+});
