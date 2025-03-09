2f6668f0c771973afa49085a633cc3dff9ee9a1b
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2025-03-09 15:07:39 +0530

    song  added@31

--- src/RedLightGreenLight.jsx
+++ src/RedLightGreenLight.jsx
@@ -21,7 +21,7 @@ const disableCopyPaste = EditorView.domEventHandlers({
   },
 });
 
-const squidGameMusic = "/public/images/squid game music.mpeg";
+const squidGameMusic = "/public/images/squid game music.mp3";
 const COMPILERX_API_URL = "https://squidgamebackend.onrender.com/compile";
 
 // Admin-provided start time and game duration (in seconds)
