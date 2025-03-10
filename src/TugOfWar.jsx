c99a31181f9d102f33ff3e9bc2c2b762359e4885
MankenaSnehitha <snehithamankena18@gmail.com>
2025-03-10 17:10:08 +0530

    time@26

--- src/TugOfWar.jsx
+++ src/TugOfWar.jsx
@@ -14,7 +14,7 @@ const TugOfWar = () => {
     useEffect(() => {
         function fetchChallengeStartTime() {
             try {
-                const simulatedStartTime = new Date("2025/03/10 17:07:00");
+                const simulatedStartTime = new Date("2025/03/10 16:59:00");
 
                 setChallengeStartTime(simulatedStartTime);
             } catch (error) {
