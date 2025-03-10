130401c255770ba65fbb6cc618036b6c6eee7ca7
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2025-03-10 20:17:12 +0530

    changed time@31

--- src/SingleAndMingle.jsx
+++ src/SingleAndMingle.jsx
@@ -56,7 +56,7 @@ const SingleAndMingle = () => {
 
   // Timer: Synchronize against a fixed deadline.
   useEffect(() => {
-    const targetTime = new Date("2025-03-10 17:00:00");
+    const targetTime = new Date("2025-03-10 21:02:00");
     const computeTimeLeft = () => {
       const now = new Date();
       const diff = Math.floor((targetTime - now) / 1000);
--- src/TugOfWar.jsx
+++ src/TugOfWar.jsx
@@ -14,7 +14,7 @@ const TugOfWar = () => {
     useEffect(() => {
         function fetchChallengeStartTime() {
             try {
-                const simulatedStartTime = new Date("2025/03/10 16:55:00");
+                const simulatedStartTime = new Date("2025/03/10 20:27:00");
 
                 setChallengeStartTime(simulatedStartTime);
             } catch (error) {
